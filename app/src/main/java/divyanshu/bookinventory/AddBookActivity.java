5ddec65adcae4fa2326677687fd8b238bc13eb34
Divyanshu <divyanshu.pundir@gmail.com>
2017-05-11 03:05:10 +0530

    feat: Created AddBookActivity

---
+++ app/src/main/java/divyanshu/bookinventory/AddBookActivity.java
@@ -0,0 +1,44 @@
+package divyanshu.bookinventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
+
+public class AddBookActivity extends AppCompatActivity {
+
+    EditText et_book_name;
+    RatingBar r_rating;
+    int type;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+
+        et_book_name = (EditText) findViewById(R.id.et_book_name);
+        r_rating = (RatingBar) findViewById(R.id.r_rating);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_book, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //End the activity
+        if (item.getItemId() == R.id.m_save_data)
+            finish();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onRadioClick(View view) {
+    }
+}
--- app/src/main/java/divyanshu/bookinventory/MainActivity.java
+++ app/src/main/java/divyanshu/bookinventory/MainActivity.java
@@ -1,8 +1,10 @@
 package divyanshu.bookinventory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.m_add_sample_data:
+                return true;
+            case R.id.m_add_book:
+                Intent intent = new Intent(MainActivity.this, AddBookActivity.class);
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
