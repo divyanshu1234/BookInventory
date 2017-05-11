3a45feeff8d4fde3c4b49055b1db503765376e12
Divyanshu <divyanshu.pundir@gmail.com>
2017-05-11 16:31:40 +0530

    feat: Added final code

--- app/src/main/java/divyanshu/bookinventory/MainActivity.java
+++ app/src/main/java/divyanshu/bookinventory/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
