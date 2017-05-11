5cad68d27be3527a6b9a970be3f51ca01a7ac2ba
Divyanshu <divyanshu.pundir@gmail.com>
2017-05-11 11:44:59 +0530

    feat: Step 1 - Create MainActivity to show data

--- app/src/main/java/divyanshu/bookinventory/MainActivity.java
+++ app/src/main/java/divyanshu/bookinventory/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
     }
 }
